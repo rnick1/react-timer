d2514ab6821393a7031d3e989c25e34b36b602d2
rnick1 <rnick0260@gmail.com>
2021-07-27 14:51:16 -0700

    Added effect hook.

--- react-timer/src/timer.js
+++ react-timer/src/timer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.css';
 
 const [second, setSecond] = useState('00');
@@ -6,8 +6,29 @@ const [minute, setMinute] = useState('00');
 const [isActive, setIsActive] = useState(false);
 const [counter, setCounter] = useState(0);
 
-
 const Timer = () => {
+
+    useEffect(() => {
+        let intervalId;
+    
+        if (isActive) {
+          intervalId = setInterval(() => {
+            const secondCounter = counter % 60;
+            const minuteCounter = Math.floor(counter / 60);
+    
+            const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}`: secondCounter;
+            const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}`: minuteCounter;
+    
+            setSecond(computedSecond);
+            setMinute(computedMinute);
+    
+            setCounter(counter => counter + 1);
+          }, 1000)
+        }
+    
+        return () => clearInterval(intervalId);
+      }, [isActive, counter])
+
     return (
         <div className="container">
             <div className="time">
@@ -16,7 +37,7 @@ const Timer = () => {
                 <span className="second">{second}</span>
             </div>
             <div className="buttons">
-                <button onClick={() => setIsActive(true)} className="start">Start</button>
+                <button onClick={() => setIsActive(!isActive)} className="start">{isActive ? "Pause" : "Start"}</button>
                 <button onClick={() => null} className="reset">Reset</button>
             </div>
             </div>
@@ -24,3 +45,19 @@ const Timer = () => {
 }
 
 export default Timer;
+
+/*
+From article by Adesile Emmanuel:
+"Lets break down what's going on in the useEffect.
+
+Toggle the start button value(Start or Pause) based on isActive state.
+We only run the setInterval function if isActive is true.
+secondCounter is calculated by getting the remainder of counter divided by 60 - using the modulo operator (%).
+minuteCounter is calculated by dividing the counter by 60 and rounding it down using Math.floor.
+We append an extra zero to the second and minute values so that we always have 2 digits.
+We update the second and minute states using the computedMinute and computedSecond values.
+count is also increased by 1 every second the effect runs.
+We return a cleanup function to clear the interval when the effect stops running.
+Lastly, we add the isActive and counter state to the dependency array. This ensures that the effect only runs when either of them changes.""
+
+*/
