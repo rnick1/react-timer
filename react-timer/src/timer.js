05d94663d4ce4fd7b459fdcdbcad959b74561be3
rnick1 <rnick0260@gmail.com>
2021-07-27 14:39:57 -0700

    Added state hook.

--- react-timer/src/timer.js
+++ react-timer/src/timer.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
+
+const [second, setSecond] = useState('00');
+const [minute, setMinute] = useState('00');
+const [isActive, setIsActive] = useState(false);
+const [counter, setCounter] = useState(0);
+
 
 const Timer = () => {
     return (
         <div className="container">
             <div className="time">
-                <span className="minute">00</span>
+                <span className="minute">{minute}</span>
                 <span>:</span>
-                <span className="second">00</span>
+                <span className="second">{second}</span>
             </div>
             <div className="buttons">
-                <button onClick={() => null} className="start">Start</button>
+                <button onClick={() => setIsActive(true)} className="start">Start</button>
                 <button onClick={() => null} className="reset">Reset</button>
             </div>
             </div>
