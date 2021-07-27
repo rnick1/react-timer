e6b493a4f0692e37c297a7ffeb8aba989f9398b8
rnick1 <rnick0260@gmail.com>
2021-07-27 11:52:33 -0700

    Added timer component and stylesheet.

---
+++ react-timer/src/timer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style';
+
+const Timer = () => {
+    return (
+        <div className="container">
+            <div className="time">
+                <span className="minute">00</span>
+                <span>:</span>
+                <span className="second">00</span>
+            </div>
+            <div className="buttons">
+                <button onClick={() => null} className="start">Start</button>
+                <button onClick={() => null} className="reset">Reset</button>
+            </div>
+            </div>
+    )
+}
+
+export default Timer;
